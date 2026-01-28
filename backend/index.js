f4f7d15d71a8deff59a7c1447a259b7a4a6aaa4e
Anurag6523 <amananurag.20@gmail.com>
2026-01-28 14:04:18 +0530

    basic-setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+
+
+//mongodb+srv://amananurag20:YlnMdLa8jS8FTqJw@cluster0.oib3vus.mongodb.net/
+app.listen(5000, () => {
+    console.log("Server is running on port 3000");
+});
